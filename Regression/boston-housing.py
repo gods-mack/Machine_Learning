6b7bc400129b22696c64fff79924ef9edbad427a
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-04-19 23:53:42 +0530

    Create boston-housing.py

---
+++ Regression/boston-housing.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score 
